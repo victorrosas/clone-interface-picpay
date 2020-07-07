76fa406d55066091aded43ee4afd79268ad8c693
Victor Rosas <victor.rosas.96@hotmail.com>
2020-07-07 01:56:37 -0300

    Criação da primeira tela

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Navigation from './Navigations';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+}
+
+
